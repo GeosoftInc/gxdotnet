f4a5eceeb49c88e84236b74e4ee4750522260a5b
Jun.Guan-seeq <jun.guan@seequent.com>
2024-11-12 17:06:41 -0500

    2024.2 - 1. Restore removed files 2. Set up solution for x86

--- examples/XToolControl/AssemblyInfo.cs
+++ examples/XToolControl/AssemblyInfo.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+[assembly: AssemblyVersionAttribute("1.0.0.1")]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
